caa7aadf44eee03e2e8673d15ad978e6a8ba61ca
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2026-01-14 17:36:39 -0500

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use zero2prod::run;
 
-async fn health_check() -> impl Responder {
-HttpResponse::Ok()
-}
-
-
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
-}
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-}
-#[cfg(test)]
-mod tests {
-use crate::health_check;
-#[tokio::test]
-async fn health_check_succeeds() {
-let response = health_check().await;
-// This requires changing the return type of `health_check`
-// from `impl Responder` to `HttpResponse` to compile
-// You also need to import it with `use actix_web::HttpResponse`!
-assert!(response.status().is_success())
-}
+    run().await
 }
