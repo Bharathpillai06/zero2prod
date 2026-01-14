de6b9b900ceeb355a59ccd65836cb881c1f05eec
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2026-01-14 17:00:22 -0500

    :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+HttpResponse::Ok().finish()
+}
+
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            // health check endpoint
+            .route("/health_check", web::get().to(health_check))
+            // greeting endpoints
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
