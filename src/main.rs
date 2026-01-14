578dd2d560242a3d762181c24f8779b762375eae
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2026-01-14 17:08:02 -0500

    tests and args added/changed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
-async fn health_check(req: HttpRequest) -> impl Responder {
-HttpResponse::Ok().finish()
+async fn health_check() -> impl Responder {
+HttpResponse::Ok()
 }
 
 
@@ -14,13 +14,21 @@ async fn greet(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            // health check endpoint
             .route("/health_check", web::get().to(health_check))
-            // greeting endpoints
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
+}
+#[cfg(test)]
+mod tests {
+use crate::health_check;
+#[tokio::test]
+async fn health_check_succeeds() {
+let response = health_check().await;
+// This requires changing the return type of `health_check`
+// from `impl Responder` to `HttpResponse` to compile
+// You also need to import it with `use actix_web::HttpResponse`!
+assert!(response.status().is_success())
+}
 }
